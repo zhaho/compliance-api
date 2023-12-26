b8e5a0831bb2bf7f82e00283c8292dad5966e041
Kenneth Ros <zhaho.ros@gmail.com>
2023-12-26 20:58:56 +0100

    Update

--- src/main.py
+++ src/main.py
@@ -4,4 +4,24 @@ app = FastAPI()
 
 @app.get("/")
 def index():
-    return {"hello": "world!"}
+    return [{"hello": "world!"}]
+
+@app.post("/api/hosts", summary="Create host")
+def index():
+    return [{"hosts": "info"}]
+
+@app.get("/api/hosts", summary="Get all hosts")
+def index():
+    return [{"hosts": "info"}]
+
+@app.get("/api/hosts/{host_id}", summary="Get host")
+def index():
+    return [{"hosts": "info"}]
+
+@app.delete("/api/hosts/{host_id}", summary="Delete host")
+def index():
+    return [{"hosts": "info"}]
+
+@app.put("/api/hosts", summary="Update host")
+def index():
+    return [{"hosts": "info"}]
