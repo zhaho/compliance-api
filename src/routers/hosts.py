1aa10ba0feaed551303f589bcc95adb8d0191290
Kenneth Ros <zhaho.ros@gmail.com>
2023-12-29 11:22:24 +0100

    Update

--- src/routers/hosts.py
+++ src/routers/hosts.py
@@ -18,10 +18,50 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 class HostRequest(BaseModel):
-    name: str = Field(min_length=3)
-    when_changed: str = Field(min_length=1)
-    when_created: str = Field(min_length=1)
+    hostname: str = Field(min_length=3)
+    main_service: str = Field(min_length=1)
+    environment: str = Field(min_length=1)
+    owner_email: str = Field(min_length=9)
+    team_id: int = Field(gt=0)
 
 @router.get("/api/hosts", status_code=status.HTTP_200_OK, tags=['Hosts'])
 async def get_all_hosts(db: db_dependency):
     return db.query(Hosts).all()
+
+@router.get("/api/hosts/{host_id}", status_code=status.HTTP_200_OK, tags=['Hosts'])
+async def get_host(db: db_dependency, host_id: int = Path(gt=0)):
+    host_model = db.query(Hosts).filter(Hosts.id == host_id).first()
+    if host_model is not None:
+        return host_model
+    raise HTTPException(status_code=404, detail='Host not found.')
+
+@router.post("/api/hosts", status_code=status.HTTP_201_CREATED, tags=['Hosts'])
+async def create_host(db: db_dependency, host_request: HostRequest):
+    host_model = Hosts(**host_request.model_dump())
+    db.add(host_model)
+    db.commit()
+
+@router.put("/api/hosts/{host_id}", status_code=status.HTTP_204_NO_CONTENT, tags=['Hosts'])
+async def update_host(db: db_dependency, host_request: HostRequest,
+                      host_id: int = Path(gt=0)):
+
+    host_model = db.query(Hosts).filter(Hosts.id == host_id).first()
+    if host_model is None:
+        raise HTTPException(status_code=404, detail='Host not found.')
+
+    host_model.hostname = host_request.hostname
+    host_model.main_service = host_request.main_service
+    host_model.environment = host_request.environment
+    host_model.owner_email = host_request.owner_email
+    host_model.team_id = host_request.team_id
+
+    db.add(host_model)
+    db.commit()
+
+@router.delete("/api/hosts/{host_id}", status_code=status.HTTP_204_NO_CONTENT, tags=['Hosts'])
+async def delete_host(db: db_dependency, host_id: int = Path(gt=0)):
+    host_model = db.query(Hosts).filter(Hosts.id == host_id).first()
+    if host_model is None:
+        raise HTTPException(status_code=404, detail='Host not found.')
+    db.query(Hosts).filter(Hosts.id == host_id).delete()
+    db.commit()
--- src/routers/teams.py
+++ src/routers/teams.py
@@ -25,3 +25,40 @@ class TeamRequest(BaseModel):
 @router.get("/api/teams", status_code=status.HTTP_200_OK, tags=['Teams'])
 async def get_all_teams(db: db_dependency):
     return db.query(Teams).all()
+
+
+@router.get("/api/teams/{team_id}", status_code=status.HTTP_200_OK, tags=['Teams'])
+async def get_team(db: db_dependency, team_id: int = Path(gt=0)):
+    team_model = db.query(Teams).filter(Teams.id == team_id).first()
+    if team_model is not None:
+        return team_model
+    raise HTTPException(status_code=404, detail='Team not found.')
+
+@router.post("/api/teams", status_code=status.HTTP_201_CREATED, tags=['Teams'])
+async def create_team(db: db_dependency, team_request: TeamRequest):
+    team_model = Teams(**team_request.model_dump())
+    db.add(team_model)
+    db.commit()
+
+@router.put("/api/teams/{team_id}", status_code=status.HTTP_204_NO_CONTENT, tags=['Teams'])
+async def update_team(db: db_dependency, team_request: TeamRequest,
+                      team_id: int = Path(gt=0)):
+
+    team_model = db.query(Teams).filter(Teams.id == team_id).first()
+    if team_model is None:
+        raise HTTPException(status_code=404, detail='Team not found.')
+
+    team_model.name = team_request.name
+    team_model.when_changed = team_request.when_changed
+    team_model.when_created = team_request.when_created
+
+    db.add(team_model)
+    db.commit()
+
+@router.delete("/api/teams/{team_id}", status_code=status.HTTP_204_NO_CONTENT, tags=['Teams'])
+async def delete_team(db: db_dependency, team_id: int = Path(gt=0)):
+    team_model = db.query(Teams).filter(Teams.id == team_id).first()
+    if team_model is None:
+        raise HTTPException(status_code=404, detail='Team not found.')
+    db.query(Teams).filter(Teams.id == team_id).delete()
+    db.commit()
